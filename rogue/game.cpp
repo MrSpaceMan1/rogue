3b86c354ecdcc448ee573651996c97c28461b0d0
MrSpaceMan1 <bartek99q@gmail.com>
2026-01-16 21:36:16 +0100

    basic player with movement every 100ms

--- rogue/game.cpp
+++ rogue/game.cpp
@@ -8,6 +8,58 @@ using namespace blit;
 //
 // setup your game here
 //
+
+
+class Player {
+    u_int8_t size = 4;
+    u_int32_t x, y = 0;
+    u_int32_t lastFrameTime = 0; // in ms
+    u_int32_t timeElapsed = 0; // in ms
+    u_int32_t updateSpan = 100; // in ms
+
+    public:
+        void render(uint32_t time, Surface* scr);
+        void update(uint32_t time);
+};
+
+void Player::render(uint32_t time, Surface* scr){
+    Surface screen = *scr;
+    screen.pen = Pen(0, 0, 0);
+    screen.clear();
+
+    screen.pen = Pen(255, 255, 255);
+    screen.rectangle(Rect(Point(this->x, this->y), Point(this->x+this->size, this->y+this->size)));
+    return;
+};
+
+void Player::update(u_int32_t time){
+    u_int32_t timeElapsedLastTick = time - this->lastFrameTime;
+    this->lastFrameTime = time;
+    this->timeElapsed += timeElapsedLastTick;
+
+    if (this->timeElapsed < this->updateSpan){
+        return;
+    } else {
+        this->timeElapsed -= this->updateSpan;
+    }
+
+    if(buttons & Button::DPAD_RIGHT){
+        this->x += this->size;
+    } 
+    else if (buttons & Button::DPAD_LEFT){
+        this->x -= this->size;
+        
+    }
+    else if (buttons & Button::DPAD_UP){
+        this->y -= this->size;
+        
+    }
+    else if (buttons & Button::DPAD_DOWN){
+        this->y += this->size;
+    }
+};
+
+Player globalPlayer = Player();
 void init() {
     set_screen_mode(ScreenMode::hires);
 }
@@ -22,15 +74,18 @@ void init() {
 void render(uint32_t time) {
 
     // clear the screen -- screen is a reference to the frame buffer and can be used to draw all things with the 32blit
-    screen.clear();
+    // screen.clear();
 
     // draw some text at the top of the screen
-    screen.alpha = 255;
-    screen.mask = nullptr;
-    screen.pen = Pen(255, 255, 255);
-    screen.rectangle(Rect(0, 0, 320, 14));
-    screen.pen = Pen(0, 0, 0);
-    screen.text("Hello 32blit!", minimal_font, Point(5, 4));
+    // screen.alpha = 255;
+    // screen.mask = nullptr;
+    // screen.pen = Pen(255, 255, 255);
+    // screen.rectangle(Rect(0, 0, 320, 14));
+    // screen.pen = Pen(0, 0, 0);
+    // screen.text("Hello 32blit!", minimal_font, Point(5, 4));
+
+    globalPlayer.render(time, &screen);
+
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -41,4 +96,5 @@ void render(uint32_t time) {
 // amount if milliseconds elapsed since the start of your game
 //
 void update(uint32_t time) {
+    globalPlayer.update(time);
 }
